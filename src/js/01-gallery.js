74e400d2f17df66b03e6630a6fce47ce75977ba2
Karol Trombala <ktrombala@gmail.com>
2023-06-25 21:28:49 +0200

    01-gallery.js
    add code and imports

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+
+const galleryMap = galleryItems
+	.map((item) => {
+		galleryEl.insertAdjacentHTML(
+			"beforeend",
+			`<div class="gallery__item">
+      <a class="gallery__link" href="${item.original}">
+      <img 
+      class="gallery__image"
+      src="${item.preview}"
+      alt="${item.description}"
+      />
+      </a>
+      </div`
+		);
+	})
+	.join("");
+
+const lightbox = new SimpleLightbox(".gallery a", {
+	captionsData: "alt",
+	captionDelay: 250,
+});

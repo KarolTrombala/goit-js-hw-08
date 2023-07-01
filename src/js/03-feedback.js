6fe0874add58761f085914c8b8e4a916dcd294cc
Karol Trombala <ktrombala@gmail.com>
2023-07-01 19:47:28 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+const throttle = require('lodash.throttle');
 
+const formEl = document.querySelector('.feedback-form');
+const emailEl = document.querySelector('input[name="email"]');
+const messageEl = document.querySelector('textarea[name="message"]');
+const LOCALSTORAGE_KEY = 'feedbak-form-state';
+
+const data = {
+  email: '',
+  message: '',
+};
+
+const saveData = () => {
+  data.email = emailEl.value;
+  data.message = messageEl.value;
+
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(data));
+};
+
+formEl.addEventListener('input', throttle(saveData, 500));
+
+const pageReload = () => {
+  const storageStatus = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+
+  if (storageStatus) {
+    emailEl.value = storageStatus.email;
+    messageEl.value = pstorageStatus.message;
+    data.email = storageStatus.email;
+    data.message = storageStatus;
+  } else {
+    emailEl.value = '';
+    messageEl.value = '';
+  }
+};
+
+formEl.addEventListener('submit', e => {
+  e.preventDefault();
+  e.currentTarget.reset();
+  saveData();
+});
